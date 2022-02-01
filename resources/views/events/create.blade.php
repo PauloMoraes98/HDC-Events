2537e2aa46973a8027ea7b1b981ecc28b8c56b6a
Paulo Cesar <paulo.csm_@hotmail.com>
2022-01-31 23:21:12 -0300

    Search in Laravel

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -13,22 +13,26 @@
             </div>
             <div class="form-group">
                 <label for="title"> Evento: </label>
-                <input type="text" class="form-control" id="title" name="title" placeholder="Nome do evento">                  
+                <input type="text" class="form-control" id="title" name="title" placeholder="Nome do evento">
+            </div>
+            <div class="form-group">
+                <label for="date"> Data do evento: </label>
+                <input type="date" class="form-control" id="date" name="date">
             </div>
             <div class="form-group">
                 <label for="title"> Cidade: </label>
-                <input type="text" class="form-control" id="city" name="city" placeholder="Local do evento">                  
+                <input type="text" class="form-control" id="city" name="city" placeholder="Local do evento">
             </div>
             <div class="form-group">
                 <label for="title"> O evento é privado? </label>
                 <select name="private" id="private" class="form-control">
                     <option value="0"> Não </option>
                     <option value="1"> Sim </option>
-                </select>    
+                </select>
             </div>
             <div class="form-group">
                 <label for="title"> Descrição: </label>
-                   <textarea name="description" id="description" class="form-control" placeholder="O que vai acontecer no evento?"></textarea>   
+                   <textarea name="description" id="description" class="form-control" placeholder="O que vai acontecer no evento?"></textarea>
             </div>
             <div class="form-group">
                 <label for="title"> Adicione itens de infraestrutura: </label>
