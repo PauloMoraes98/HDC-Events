96e37654c8c2eea21079ae5739ccb5a1895f519f
Paulo Cesar <paulo.csm_@hotmail.com>
2022-02-01 01:07:55 -0300

    Authentication in Laravel (login/registration)

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -11,10 +11,10 @@
 
         <!-- CSS bootstrap -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-        
+
         <!-- CSS da aplicação -->
         <link rel="stylesheet" href="/css/styles.css">
-        
+
         <!-- JS da aplicação -->
         <script src="/js/scripts.js"></script>
     </head>
@@ -32,12 +32,31 @@
                         <li class="nav-item">
                             <a href="/events/create" class="nav-link">Criar Eventos</a>
                         </li>
-                        <li class="nav-item">
-                            <a href="/" class="nav-link">Entrar</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="/" class="nav-link">Cadastrar</a>
-                        </li>
+                        @auth
+                            <li class="nav-item">
+                                <a href="/dashboard" class="nav-link">Meus eventos</a>
+                            </li>
+                            <li class="nav-item">
+                                <form action="/logout" method="POST">
+                                    @csrf
+                                    <a href="/logout"
+                                        class="nav-link"
+                                        onclick="event.preventDefault();
+                                            this.closest('form').submit();"
+                                    >
+                                        Sair
+                                    </a>
+                                </form>
+                            </li>
+                        @endauth
+                        @guest
+                            <li class="nav-item">
+                                <a href="/login" class="nav-link">Entrar</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="/register" class="nav-link">Cadastrar</a>
+                            </li>
+                        @endguest
                     </ul>
                 </div>
             </nav>
@@ -55,7 +74,7 @@
         <footer>
             <p>HDC Events &copy; 2022</p>
         </footer>
-            <script  type = "module"  src = "https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script> 
+            <script  type = "module"  src = "https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
             <script  nomodule  src = "https://unpkg .com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
     </body>
 </html>
