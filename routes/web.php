ee0da7067c7f1106521430207bea44552c949eac
Paulo Cesar <paulo.csm_@hotmail.com>
2022-02-02 01:31:52 -0300

    many to many relationship

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,4 @@ Route::delete('/events/{id}', [EventController::class, 'destroy']);
 Route::get('/events/edit/{id}', [EventController::class, 'edit'])->middleware('auth');
 Route::put('/events/update/{id}', [EventController::class, 'update'])->middleware('auth');
 Route::get('/dashboard', [EventController::class, 'dashboard'])->middleware('auth');
+Route::post('/events/join/{id}', [EventController::class, 'joinEvent'])->middleware('auth');
