46302c8eb3074f3fbc7f175d7246ca76cd8b38eb
Paulo Cesar <paulo.csm_@hotmail.com>
2022-02-01 23:26:50 -0300

    Data Update (CRUD: Update)

--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,6 @@ Route::get('/events/create', [EventController::class, 'create'])->middleware('au
 Route::get('/events/{id}', [EventController::class, 'show']);
 Route::post('/events', [EventController::class, 'store']);
 Route::delete('/events/{id}', [EventController::class, 'destroy']);
+Route::get('/events/edit/{id}', [EventController::class, 'edit'])->middleware('auth');
+Route::put('/events/update/{id}', [EventController::class, 'update'])->middleware('auth');
 Route::get('/dashboard', [EventController::class, 'dashboard'])->middleware('auth');
